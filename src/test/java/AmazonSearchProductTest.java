305a876832fffc490d6d8745cb49164cd4ddbdd6
SowModibo <sowmodibo62@gmail.com>
2023-04-05 20:35:24 +0200

    firefox driver used

--- src/test/java/AmazonSearchProductTest.java
+++ src/test/java/AmazonSearchProductTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -18,7 +19,7 @@ public class AmazonSearchProductTest {
     WebDriver driver;
     @BeforeMethod
     public void setup() {
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.manage().window().fullscreen();
     }
 
